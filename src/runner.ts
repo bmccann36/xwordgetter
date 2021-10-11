525935d587d11be392dc38ece704e20f5f08abfa
BRIAN MCCANN <extern.mccann@mbusa.com>
2021-10-11 09:24:44 -0400

    fix cron to be realtive to UTC

--- src/runner.ts
+++ src/runner.ts
@@ -7,7 +7,9 @@ import fs from 'fs';
 
 //? docker image handler with puppeteer
 
-Handler({ puzzleDateOverride: '2021/10/08' })
+Handler(
+  { "puzzleDateOverride": "2021/10/08"}
+  )
 
 //? s3 reader function
 // const objCreatedJson = fs.readFileSync(path.join(__dirname, '..', 'mocks/objCreated.json'))
